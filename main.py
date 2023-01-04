faf4d119fef2998d5b7aa52bd7a8cea0e058c734
Kostas Priftis <priftis.kostas1997@gmail.com>
2023-01-04 18:25:15 +0200

    Kelvin to celsius converter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import datetime as dt
 
+def kelvinToCelsius(kelvin):
+    return kelvin - 273.15
+
+
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 API_KEY = open('api_key', 'r').read()
 CITY = input("City: ")
@@ -8,5 +12,9 @@ CITY = input("City: ")
 URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY 
 
 response = requests.get(URL).json()
+    
+temperature = response['main']['temp']
+cityName = response['name']
 
-print(response)
+print(round(kelvinToCelsius(temperature)))
+print(cityName)
