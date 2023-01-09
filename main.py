996352fb5d640049a846a31f8056aa202f3444ee
Kostas Priftis <priftis.kostas1997@gmail.com>
2023-01-09 15:55:11 +0200

    extracted relevant data from api response

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import datetime as dt
 import json
+import tkinter as tk
+
+
 
 def kelvinToCelsius(kelvin):
     return kelvin - 273.15
 
-
-API_KEY = open('api_key', 'r').read()
-CITY_NAME = input("City: ")
 
 def get_weather(city,api_key):
     URL = f"http://api.openweathermap.org/data/2.5/weather?q={city} +&appid={api_key}"
@@ -18,7 +18,27 @@ def get_weather(city,api_key):
 
 
 
-weather_data = get_weather(CITY_NAME,API_KEY)  
+
+def main():
+
+    API_KEY = open('api_key', 'r').read()
+    CITY_NAME = input("City: ")
+
+    weather_data = get_weather(CITY_NAME,API_KEY)  
+
+    coord = weather_data['coord']
+    weather_desc = weather_data['weather'][0]['description']
+    temp_kelvin = weather_data['main']['temp']
+    feels_like = weather_data['main']['feels_like']
+    humidity = weather_data['main']['humidity']
+    wind_speed = weather_data['wind']['speed']
+    city_name = weather_data['name']\
+    
+    window = tk.Tk()
+    
+    window.mainloop()
 
 
-print(weather_data)
+
+if __name__ == "__main__":
+    main()
